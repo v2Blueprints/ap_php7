d3d4de6e99b291ce0f31004a67c2ab2f2621c6ec
James Vodanovich <james@jvodan.com>
2020-12-09 12:30:42 +1100

    reinstate install php modules

---
+++ packing/scripts/late/add_php_modules.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#needs to be updated to use templating and not file for php module list
+
+if test -f /tmp/packing/etc/modules/php
+ then
+   for module in `cat /tmp/packing/etc/modules/php`
+    do
+       phpenmod $module
+    done        
+ fi
